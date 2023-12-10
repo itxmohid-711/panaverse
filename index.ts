41567dbe489c8a2d722f1d4856e730a51962eead
Mohid Ali <immohidjameel47@gmail.com>
2023-12-10 11:45:00 +0500

    hello world

---
+++ index.ts
@@ -0,0 +1,56 @@
+// //check prime number 
+// function isPrime(num: number): string {
+//     if (num <= 1) {
+//         return "it is not a prime number";
+//     }
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) {
+//             return "it is not a prime number"; 
+//         }
+//     }
+//     return "it is a prime number";
+// }
+// console.log(isPrime(8)); // it is a prime number
+// let fruit1 = "apple";
+// let fruit2= "mango ";
+// let fruit3 = "banana";
+// let fruit4 = "pineaple";
+// let fruit5 = "strawberry";
+// const fruits = ["apple","mango ","banana","pineaple","strawberry"]
+// console.log("🚀 ~ file: index.ts:20 ~ fruits:", fruits)
+// methods for arra
+// array dynanmic or static
+// push //pop
+// shift(remove the first latter from aray ) //unshift()
+// const frnds = ["ali","umar","ahsan","jutt"]
+// console.log(frnds[0],"ki hall ay");
+// console.log(frnds[1],"hello"); 
+// console.log(frnds[2],"hayyyy");
+// console.log(frnds[3],"hlww");
+// loops
+// if condition us true it will run continuously
+// when it will become false it will end
+// loop should terminate
+// types ||for||while||do while
+// let fruits : string [] = ["apple,banana,mango,pineapple,cherry"];
+// let g =fruits.shift()
+// console.log("🚀 ~ file: index.ts:37 ~ g:", g)
+/*let fruits =[ 'apple' , 'mango' , 'leamon' ];
+fruits.splice(2,1, 'cherry');
+console.log("🚀 ~ file: index.ts:39 ~ fruits:", fruits);*/
+// const fruits =[ 'apple' , 'mango' , 'leamon' ];
+// fruits.slice(0,1);
+// console.log(fruits);
+let students=["ali" ,"umar" ,"shazad","mani"];
+for(
+    let x=0; x< students.length ; x++
+)
+{
+    console.log(students [students])
+}
+
+
+
+
+
+export{}
